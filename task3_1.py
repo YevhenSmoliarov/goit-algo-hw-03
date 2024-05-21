cdeb5ab730b18a6114130e92a092b9c292e581a7
Yevhen Smoliarov <smoliarov.yevhen@gmail.com>
2024-05-21 16:08:52 +0100

    fixed the code

--- task3_1.py
+++ task3_1.py
@@ -8,19 +8,23 @@ def copy_files(source_dir, dest_dir):
         os.makedirs(dest_dir)
     
     # Рекурсивне копіювання файлів
-    for root, dirs, files in os.walk(source_dir):
-        for file in files:
+    for item in os.listdir(source_dir):
+        source_item_path = os.path.join(source_dir, item)
+        if os.path.isdir(source_item_path):
+            # Якщо це піддиректорія, рекурсивно копіюємо файли з неї
+            copy_files(source_item_path, dest_dir)
+        else:
             # Повний шлях до вихідного файлу
-            source_file_path = os.path.join(root, file)
+            source_file_path = source_item_path
             # Розширення файлу
-            file_extension = os.path.splitext(file)[1]
+            file_extension = os.path.splitext(item)[1]
             # Шлях до піддиректорії вихідної директорії
-            dest_subdir = os.path.join(dest_dir, file_extension[1:])
+            dest_subdir = os.path.join(dest_dir, file_extension[1:] if file_extension else 'no_extension')
             # Перевірка чи існує піддиректорія, якщо ні - створюємо
             if not os.path.exists(dest_subdir):
                 os.makedirs(dest_subdir)
             # Шлях до вихідного файлу
-            dest_file_path = os.path.join(dest_subdir, file)
+            dest_file_path = os.path.join(dest_subdir, item)
             try:
                 # Копіюємо файл у відповідну піддиректорію
                 shutil.copy2(source_file_path, dest_file_path)
@@ -30,12 +34,12 @@ def copy_files(source_dir, dest_dir):
 
 def main():
     # Перевірка чи передано правильну кількість аргументів
-    if len(sys.argv) < 3:
-        print("Usage: python script.py source_dir dest_dir")
+    if len(sys.argv) < 2:
+        print("Usage: python script.py source_dir [dest_dir]")
         sys.exit(1)
     
     source_dir = sys.argv[1]
-    dest_dir = sys.argv[2]
+    dest_dir = sys.argv[2] if len(sys.argv) > 2 else 'dist'
 
     # Виклик функції для копіювання файлів
     copy_files(source_dir, dest_dir)
